6594fa7242c1c5403a5d891b27cb79fd2cdf35f7
NYejin <kidjin36@gmail.com>
2026-07-22 17:41:42 +0900

    admin 권한 없는 계정 페이지 접근제한

--- coliving/src/app/admin/layout.tsx
+++ coliving/src/app/admin/layout.tsx
@@ -1,12 +1,15 @@
 import { AdminSidebar } from "@/components/AdminSidebar";
+import { AdminGate } from "./AdminGate";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="wrap" style={{ paddingTop: 28, paddingBottom: 60 }}>
-      <div className="host-layout">
-        <AdminSidebar />
-        <div style={{ minWidth: 0 }}>{children}</div>
-      </div>
+      <AdminGate>
+        <div className="host-layout">
+          <AdminSidebar />
+          <div style={{ minWidth: 0 }}>{children}</div>
+        </div>
+      </AdminGate>
     </div>
   );
 }
